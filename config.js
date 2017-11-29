16a1869a927a05c796af7bd0dbfec4f0277401d9
MatPA1 <martin.sanderson589@gmail.com>
2017-11-29 21:24:21 +0000

    Update config.js

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ module.exports = {
 	// If true, it will delete any message that is not a command from the master channel.
 	// Leave this on to keep your master channel tidy.
 	// This also requires the "manage messages" permission
-	disallow_talking: true
+	disallow_talking: false
 };
 
 // "Add to server" link:
